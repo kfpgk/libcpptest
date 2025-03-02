4cf62ec2e45c035dd1bcc46f8d9b5808400b2eb7
Felix Kopp <felix@goodkopp.net>
2025-03-02 13:46:13 +0100

    Import package libcpplog inside cmake config file

--- libcpptest/config/cpptestConfig.cmake
+++ libcpptest/config/cpptestConfig.cmake
@@ -1,5 +1,12 @@
-set(LIBCPPTEST_INCLUDE_DIRS ${LIBCPPTEST_DIR}/include)
-set(LIBCPPTEST_LIBRARY_DIR ${LIBCPPTEST_DIR}/lib)
+find_package(LIBCPPLOG
+    NAMES
+        cpplog
+        libcpplog
+    REQUIRED
+)
+
+set(LIBCPPTEST_INCLUDE_DIRS ${LIBCPPTEST_DIR}/include ${LIBCPPLOG_DIR}/include)
+set(LIBCPPTEST_LIBRARY_DIR ${LIBCPPTEST_DIR}/lib ${LIBCPPLOG_DIR}/lib)
 set(LIBCPPTEST_LIB_NAME libcpptest)
 
 if (WIN32)
